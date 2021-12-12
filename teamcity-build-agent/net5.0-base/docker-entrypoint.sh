c34f03fb282e4c268f7d7fab8c0b3f8f11c33fe6
kosar <87856959+1k-off@users.noreply.github.com>
2021-12-13 00:51:51 +0200

    Feature/teamcity agent base (#1)
    * Added teamcity build agent base

---
+++ teamcity-build-agent/net5.0-base/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo service docker start
+sudo service docker status
+git config --global user.email "buildagent@teamcity"
+git config --global user.name "TC BuildAgent"
+exec $@
