e79f157a7406227afbdffed4034cadaf90e5b49b
kosar <87856959+1k-off@users.noreply.github.com>
2022-08-08 16:30:59 +0300

    Update docker-entrypoint.sh

--- teamcity-build-agent/base/docker-entrypoint.sh
+++ teamcity-build-agent/base/docker-entrypoint.sh
@@ -2,6 +2,12 @@
 
 sudo service docker start
 sudo service docker status
+# configure git
 git config --global user.email "buildagent@teamcity"
 git config --global user.name "TC BuildAgent"
+# disable known hosts checks # https://www.jetbrains.com/help/teamcity/ssh-agent.html#Disabling+SSH+Host+Key+Checking
+cat << EOF > ~/.ssh/config
+Host *
+    StrictHostKeyChecking no
+EOF
 exec $@
