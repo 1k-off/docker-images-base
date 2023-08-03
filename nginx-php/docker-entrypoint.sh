1d3954fa08c22fbbcda8a4f6ce8ee9b04507ac19
kosar <bogdan.kosarevskyi@gmail.com>
2023-08-03 17:23:20 +0300

    Switch new php versions to alpine

--- nginx-php/docker-entrypoint.sh
+++ nginx-php/docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefail
-install() {
-    chown -R www-data:www-data /var/www/html
-    find /var/www/html -type f -exec chmod 644 {} \;
-    find /var/www/html -type d -exec chmod 755 {} \;
-
-    rm -f /etc/nginx/sites-enabled/default
-    rm -f /etc/nginx/sites-available/default
-}
-
-install
+chown -R www-data:www-data /var/www/html
+find /var/www/html -type f -exec chmod 644 {} \;
+find /var/www/html -type d -exec chmod 755 {} \;
+rm -f /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-available/default
 exec $@
